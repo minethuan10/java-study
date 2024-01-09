cbf68a2c0219c05082fa004b9f427993a7b9e990
Vu Duc Thuan Tran <vdtr2808@gmail.com>
2024-01-09 01:51:04 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Example 1
+        int age=19;
+        double grade=3.0;
+        char gender='M';
+        boolean programmer=true;
+
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(grade);
+        System.out.println(programmer);
+
+        // Example 2
+        String myName="Thuan";
+        char firstname=myName.charAt(0);
+        System.out.println(myName +" first character of this is "+ firstname);
+    }
+}
