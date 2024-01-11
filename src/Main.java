c4e0641f709e09182527decc9e336364268e6a56
VuDucThuanTran <vutranducthuan@gmail.com>
2024-01-11 18:50:26 -0500

    understand input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -18,5 +19,12 @@ public class Main {
         String myName="Thuan";
         char firstname=myName.charAt(0);
         System.out.println(myName +" first character of this is "+ firstname);
+        // Example 3
+        Scanner input= new Scanner(System.in);
+        //the line 23 above u need it because it a way to make sure that your computer can access into the data
+        System.out.println("what age did u want to change: ");
+        age=input.nextInt();
+        System.out.println("Your age have been updated. You are "+age);
+
     }
 }
